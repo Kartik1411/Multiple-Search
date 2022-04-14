5a9955bb8ae9f895eb56399be1b4b2894e5f1dab
kartik1411 <kartikvarshney1411@gmail.com>
2022-04-14 12:00:26 +0530

    useContext added

--- multiple-search/src/App.js
+++ multiple-search/src/App.js
@@ -24,7 +24,7 @@ function App() {
   },[])
 
   return (
-    <UsersContext.Provider value={[users]}>
+    <UsersContext.Provider value={[users, setUsers]}>
       <div className="App">
         <MultiSearch />
       </div>
